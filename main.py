db7a304012a0c048d8fbfcafdcaff5fdeacfcf9a
Rick Getz <richardgetziii@gmail.com>
2022-12-22 22:55:34 -0500

    easy cleanup script working

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from pyarr import SonarrAPI
 from pyarr import RadarrAPI
+import json
 
 
-# You can find your API key in Settings > General.
-api_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+secrets = None
+with open("secrets.json", "r") as f:
+    secrets = json.load(f)
+def shows():
+    # Instantiate SonarrAPI Object
+    sonarr = SonarrAPI(secrets["sonarr"]["host"], secrets["sonarr"]["key"])
 
-# Instantiate SonarrAPI Object
-sonarr = SonarrAPI(host_url, api_key)
+    # Get and print TV Shows
+    for series in sonarr.get_series():
+        if (not series["monitored"] and series["episodeFileCount"] == 0):
+            print(f'Removing {str(series["title"])} from monitoring.')
+            sonarr.del_series(id_=series["id"], delete_files=True)
+        
+def movies():
+    # Instantiate SonarrAPI Object
+    radarr = RadarrAPI(secrets["radarr"]["host"], secrets["radarr"]["key"])
 
-# Get and print TV Shows
-print(sonarr.get_series())
+    # Get and print TV Shows
+    for movie in radarr.get_movie():
+        # print(movie["title"], movie["monitored"], movie["id"], movie["hasFile"])
+        if (not movie["monitored"] and not movie["hasFile"]):
+            print(f'Removing {str(movie["title"])} from monitoring.')
+            radarr.del_movie(id_=movie["id"], delete_files=True, add_exclusion=False)
+
+shows()
+movies()
